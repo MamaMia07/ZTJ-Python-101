985310826e6a482075372474adae973080d4463b
MamaMia07 <kokoszkam@gazeta.pl>
2023-04-22 22:22:04 +0200

    creating functions

--- TickTackToe.py
+++ TickTackToe.py
@@ -1,13 +1,44 @@
 import random
+from tabulate import tabulate
 
 print("""
 Welcome to Tic-Tac-Toe game.
 Let's play!
 """)
 
-n=3 # game board size
+def print_nice_table(data_set, board_size):
+    ''' print 1D list in tabular format '''
+    k=0
+    table = [['' for x in range(board_size)] for y in range(board_size)]
+    for i in range(board_size):
+        for j in range(board_size):
+            table[i][j]= data_set[k]
+            k += 1
+    print(tabulate(table, tablefmt='grid')) 
 
+
+def is_the_winner(data_set,board_size, mark):
+    ''' check if 3 x mark (e.g.'X' or 'O') in rows, columns,
+        main diagonal and secondary diagonal of game board '''
+    m = len(data_set)
+    n = board_size
+    if all(field == mark for field in data_set[0:n]) \
+        or all(field == mark for field in data_set[:2*n]) \
+        or all(field == mark for field in data_set[2*n:3*n]) \
+        or all(field == mark for field in data_set[0:m:3]) \
+        or all(field == mark for field in data_set[1:m:3]) \
+        or all(field == mark for field in data_set[2:m:3]) \
+        or all(field == mark for field in data_set[0:m:4]) \
+        or all(field == mark for field in data_set[2:m-1:2]):
+        return True
+    else:
+        return False
+        
+    
+# game start
 while True:
+    n=3 # game board size
+
     # generating field indexes
     fields = []
     for i in range(n):
@@ -18,64 +49,38 @@ while True:
 
     # printing the game board
     while True:
-        k=0
-        print('  -- '*3)
-        for i in range(n):
-            for j in range(n):
-                print(f'| {fields[k]} ',end='')
-                k += 1
-            print('|')
-            print('  -- '*3)
-        print('')
+        print_nice_table(fields, n)
+            
+    # checking:  is the winner?     
+        if is_the_winner(fields, n, 'X'):
+            print('Congratulations!\nYou won!')
+            break
 
-    # checking:  3 x 'X'  or  3 x 'O' 
-        m = len(fields)
-        if all(field == "X" for field in fields[0:n]) \
-            or all(field == "X" for field in fields[n:2*n]) \
-            or all(field == "X" for field in fields[2*n:3*n]) \
-            or all(field == "X" for field in fields[0:m:3]) \
-            or all(field == "X" for field in fields[1:m:3]) \
-            or all(field == "X" for field in fields[2:m:3]) \
-            or all(field == "X" for field in fields[0:m:4]) \
-            or all(field == "X" for field in fields[2:m-1:2]):
-                print("Congratulations!\nYou won!")
-                break
-
-        elif all(field == "O" for field in fields[0:n]) \
-              or all(field == "O" for field in fields[n:2*n]) \
-              or all(field == "O" for field in fields[2*n:3*n]) \
-              or all(field == "O" for field in fields[0:m:3]) \
-              or all(field == "O" for field in fields[1:m:3]) \
-              or all(field == "O" for field in fields[2:m:3]) \
-              or all(field == "O" for field in fields[0:m:4]) \
-              or all(field == "O" for field in fields[2:m-1:2]):
-                  print("Player 2 won")
-                  break
+        elif is_the_winner(fields,n, 'O'):
+            print('Player 2 won')
+            break
 
     # player selection
         player1 = ''
         while player1 not in remains:
-            player1 = input('Choose a field number: ')
-        
-        
+            player1 = input('Choose a field number: ')         
+
         remains.remove(player1)
         fields = ['X' if field == player1 else field for field in fields]
 
-        # random player 2 selection
-        if len(remains)!= 0:  # to na warunek petli
+        # random player2 selection
+        if len(remains)!= 0:  
             player2 = random.choice(remains)
             print('Player2 selected: '+ player2+'\n')
             remains.remove(player2)
             fields = ['O' if field == player2 else field for field in fields]
         else:
-            print("Draw!\nGame over")
+            print('Draw!\nGame over')
             break
-        #elif all(fields[0,4,6] or
 
-        #warunek +=2       
-     
+         
     answer = input('Do you want to play again? (y/n) ')
     print('')
     answer = answer.lstrip().lower()
-    if answer.startswith("y")==False:
+    if answer.startswith('y')==False:
        break
